539f2ca0b91f3d323d50e4eb93f8ac187bef6e95
Ariel-HS <arielherfrison@gmail.com>
2024-12-01 10:53:02 +0700

    feat: update swagger

--- backend/src/swagger.ts
+++ backend/src/swagger.ts
@@ -22,6 +22,16 @@ export const swaggerConfig: OpenAPI.Document = {
       }
     },
     schemas: {
+      UserData: {
+        type: 'object',
+        properties: {
+          token: { type: 'string' },
+          id: { type: 'string' },
+          email: { type: 'string' },
+          username: { type: 'string' },
+          fullName: { type: 'string' },
+        }
+      },
       User: {
         type: 'object',
         properties: {
@@ -34,14 +44,6 @@ export const swaggerConfig: OpenAPI.Document = {
           profilePhotoPath: { type: 'string' }
         }
       },
-      LoginRequest: {
-        type: 'object',
-        required: ['email', 'password'],
-        properties: {
-          email: { type: 'string', format: 'email' },
-          password: { type: 'string', format: 'password' }
-        }
-      },
       RegisterRequest: {
         type: 'object',
         required: ['username', 'email', 'password', 'confirmPassword'],
@@ -52,10 +54,135 @@ export const swaggerConfig: OpenAPI.Document = {
           confirmPassword: { type: 'string', format: 'password' },
           fullName: { type: 'string' }
         }
+      },
+      LoginRequest: {
+        type: 'object',
+        required: ['email', 'password'],
+        properties: {
+          email: { type: 'string', format: 'email' },
+          password: { type: 'string', format: 'password' }
+        }
+      },
+      Profile: {
+        type: 'object',
+        properties: {
+          email: { type: 'string' },
+          username: { type: 'string' },
+          fullName: { type: 'string' },
+          skills: { type: 'string' },
+          workHistory: { type: 'string' },
+          profilePhotoPath: { type: 'string' },
+          connections: { type: 'string' }
+        }
+      },
+      Chat: {
+        type: 'object',
+        properties: {
+          id: { type: 'string' },
+          timestamp: { type: 'string' },
+          fromId: { type: 'string' },
+          toId: { type: 'string' },
+          message: { type: 'string' },
+          read: { type: 'boolean' },
+          readAt: { type: 'string' }
+        }
       }
     }
   },
   paths: {
+    '/api/verify': {
+      post: {
+        tags: ['Authentication'],
+        summary: 'Verify token & Get user data',
+        security: [
+          { cookieAuth: [] }
+        ],
+        responses: {
+          '200': {
+            description: 'Token verified',
+            content: {
+              'application/json': {
+                schema: {
+                  type: 'object',
+                  properties: {
+                    success: { type: 'boolean' },
+                    message: { type: 'string' },
+                    body: { 
+                      $ref: '#/components/schemas/UserData'
+                    }
+                  }
+                }
+              }
+            }
+          },
+          '401': {
+            description: 'Failed operation'
+          }
+        }
+      }
+    },
+    '/api/logout': {
+      post: {
+        tags: ['Authentication'],
+        summary: 'Logout user',
+        responses: {
+          '200': {
+            description: 'Logged out successfully'
+          }
+        }
+      }
+    },
+    '/api/register': {
+      post: {
+        tags: ['Authentication'],
+        summary: 'Register a new user',
+        requestBody: {
+          required: true,
+          content: {
+            'application/json': {
+              schema: {
+                $ref: '#/components/schemas/RegisterRequest'
+              }
+            }
+          }
+        },
+        responses: {
+          '200': {
+            description: 'Registration Successful'
+          },
+          '400': {
+            description: 'Failed operation',
+            content: {
+              'application/json': {
+                schema: {
+                  type: 'object',
+                  properties: {
+                    success: { type: 'boolean' },
+                    message: { type: 'string' },
+                    body: { 
+                      oneOf: [
+                        {
+                          type: "object",
+                          properties: {
+                            field: {type: 'string'},
+                            message: {type: 'string'}
+                          }
+                        }, {
+                          type: null
+                        } 
+                      ]
+                    }
+                  }
+                }
+              }
+            }
+          },
+          '500': {
+            description: 'Registration failed'
+          }
+        }
+      }
+    },
     '/api/login': {
       post: {
         tags: ['Authentication'],
@@ -90,27 +217,15 @@ export const swaggerConfig: OpenAPI.Document = {
                 }
               }
             }
-          }
-        }
-      }
-    },
-    '/api/register': {
-      post: {
-        tags: ['Authentication'],
-        summary: 'Register a new user',
-        requestBody: {
-          required: true,
-          content: {
-            'application/json': {
-              schema: {
-                $ref: '#/components/schemas/RegisterRequest'
-              }
-            }
-          }
-        },
-        responses: {
-          '200': {
-            description: 'Successful registration'
+          },
+          '400': {
+            description: 'Email and password are required'
+          },
+          '401': {
+            description: 'Invalid credentials'
+          },
+          '500': {
+            description: 'Registration failed'
           }
         }
       }
@@ -134,7 +249,39 @@ export const swaggerConfig: OpenAPI.Document = {
         ],
         responses: {
           '200': {
-            description: 'Profile retrieved successfully'
+            description: 'Profile retrieved successfully',
+            content: {
+              'application/json': {
+                schema: {
+                  type: 'object',
+                  properties: {
+                    success: { type: 'boolean' },
+                    message: { type: 'string' },
+                    body: {
+                      // TODO write examples
+                      oneOf: [
+                        {
+                          $ref: '#/components/schemas/Profile'
+                        }, 
+                        {
+                          type: 'object',
+                          properties: {
+                            fullName: { type: 'string' },
+                            skills: { type: 'string' },
+                            workHistory: { type: 'string' },
+                            profilePhotoPath: { type: 'string' },
+                            connections: { type: 'string' }
+                          }
+                        }
+                      ]
+                    }
+                  }
+                }
+              }
+            }
+          },
+          '401': {
+            description: 'Invalid user'
           }
         }
       },
@@ -172,7 +319,37 @@ export const swaggerConfig: OpenAPI.Document = {
         },
         responses: {
           '200': {
-            description: 'Profile updated successfully'
+            description: 'Profile updated successfully',
+            content: {
+              'application/json': {
+                schema: {
+                  type: 'object',
+                  properties: {
+                    success: { type: 'boolean' },
+                    message: { type: 'string' },
+                    body: {
+                      type: 'object',
+                      properties: {
+                        fullName: { type: 'string' },
+                        username: { type: 'string' },
+                        skills: { type: 'string' },
+                        workHistory: { type: 'string' },
+                        profilePhotoPath: { type: 'string' }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          },
+          '400': {
+            description: 'Bad request'
+          },
+          '401': {
+            description: 'Unauthorized request'
+          },
+          '500': {
+            description: 'Failed operation'
           }
         }
       }
@@ -192,10 +369,60 @@ export const swaggerConfig: OpenAPI.Document = {
         ],
         responses: {
           '200': {
-            description: 'Search results retrieved successfully'
+            description: 'Search results retrieved successfully',
+            content: {
+              'application/json': {
+                schema: {
+                  type: 'object',
+                  properties: {
+                    success: { type: 'boolean' },
+                    message: { type: 'string' },
+                    body: {
+                      type: 'object',
+                      properties: {
+                        id: {type: 'string' },
+                        username: { type: 'string' },
+                        fullName: { type: 'string' },
+                        profilePhotoPath: { type: 'string' }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          },
+          '500': {
+            description: 'Failed to perform search'
           }
         }
       }
+    },
+    '/api/connection-status/{id}': {
+      get: {
+        tags: ['Profile'],
+        summary: 'Get profile connection status',
+        parameters: [
+          {
+            name: 'id',
+            in: 'path',
+            required: true,
+            schema: {
+              type: 'string'
+            }
+          }
+        ],
+        security: [
+          { cookieAuth: [] }
+        ],
+        responses: {
+          '200': {
+            description: 'Successful operation',
+          },
+          '401': {
+            description: 'Failed operation'
+          }
+        }
+      },
     },
     '/api/network/unconnected': {
       get: {
@@ -206,7 +433,38 @@ export const swaggerConfig: OpenAPI.Document = {
         ],
         responses: {
           '200': {
-            description: 'Unconnected users retrieved successfully'
+            description: 'Unconnected users retrieved successfully',
+            content: {
+              'application/json': {
+                schema: {
+                  type: 'object',
+                  properties: {
+                    success: { type: 'boolean' },
+                    message: { type: 'string' },
+                    body: {
+                      type: 'object',
+                      properties: {
+                        connection: {
+                          type: 'array',
+                          items: {
+                            type: 'object',
+                            properties: {
+                              id: {type: 'string' },
+                              username: { type: 'string' },
+                              fullName: { type: 'string' },
+                              profilePhotoPath: { type: 'string' }
+                            }
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          },
+          '401': {
+            description: 'Failed operation'
           }
         }
       }
@@ -219,8 +477,41 @@ export const swaggerConfig: OpenAPI.Document = {
           { cookieAuth: [] }
         ],
         responses: {
-          '200': {
-            description: 'Requested connections retrieved successfully'
+         '200': {
+            description: 'Requested connections retrieved successfully',
+            content: {
+              'application/json': {
+                schema: {
+                  type: 'object',
+                  properties: {
+                    success: { type: 'boolean' },
+                    message: { type: 'string' },
+                    body: {
+                      type: 'object',
+                      properties: {
+                        connection: {
+                          type: 'array',
+                          items: {
+                            type: 'object',
+                            properties: {
+                              id: {type: 'string' },
+                              fullName: { type: 'string' },
+                              username: { type: 'string' },
+                              skills: { type: 'string' },
+                              workHistory: { type: 'string' },
+                              profilePhotoPath: { type: 'string' }
+                            }
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          },
+          '401': {
+            description: 'Failed operation'
           }
         }
       }
@@ -234,7 +525,40 @@ export const swaggerConfig: OpenAPI.Document = {
         ],
         responses: {
           '200': {
-            description: 'Incoming requests retrieved successfully'
+            description: 'Incoming requests retrieved successfully',
+            content: {
+              'application/json': {
+                schema: {
+                  type: 'object',
+                  properties: {
+                    success: { type: 'boolean' },
+                    message: { type: 'string' },
+                    body: {
+                      type: 'object',
+                      properties: {
+                        connection: {
+                          type: 'array',
+                          items: {
+                            type: 'object',
+                            properties: {
+                              id: {type: 'string' },
+                              fullName: { type: 'string' },
+                              username: { type: 'string' },
+                              skills: { type: 'string' },
+                              workHistory: { type: 'string' },
+                              profilePhotoPath: { type: 'string' }
+                            }
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          },
+          '401': {
+            description: 'Failed operation'
           }
         }
       }
@@ -248,7 +572,40 @@ export const swaggerConfig: OpenAPI.Document = {
         ],
         responses: {
           '200': {
-            description: 'Connected users retrieved successfully'
+            description: 'Connected users retrieved successfully',
+            content: {
+              'application/json': {
+                schema: {
+                  type: 'object',
+                  properties: {
+                    success: { type: 'boolean' },
+                    message: { type: 'string' },
+                    body: {
+                      type: 'object',
+                      properties: {
+                        connection: {
+                          type: 'array',
+                          items: {
+                            type: 'object',
+                            properties: {
+                              id: {type: 'string' },
+                              fullName: { type: 'string' },
+                              username: { type: 'string' },
+                              skills: { type: 'string' },
+                              workHistory: { type: 'string' },
+                              profilePhotoPath: { type: 'string' }
+                            }
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          },
+          '401': {
+            description: 'Failed operation'
           }
         }
       }
@@ -273,6 +630,152 @@ export const swaggerConfig: OpenAPI.Document = {
         responses: {
           '200': {
             description: 'Connection request sent successfully'
+          },
+          '400': {
+            description: 'Bad request'
+          },
+          '401': {
+            description: 'Failed operation'
+          }
+        }
+      }
+    },
+    '/api/accept-request/{id}': {
+      post: {
+        tags: ['Network'],
+        summary: 'Accept incoming connection request',
+        security: [
+          { cookieAuth: [] }
+        ],
+        parameters: [
+          {
+            name: 'id',
+            in: 'path',
+            required: true,
+            schema: {
+              type: 'string'
+            }
+          }
+        ],
+        responses: {
+          '200': {
+            description: 'Connection accepted'
+          },
+          '400': {
+            description: 'Bad request'
+          },
+          '401': {
+            description: 'Unauthorized request'
+          },
+          '500': {
+            description: 'Failed operation'
+          }
+        }
+      }
+    },
+    '/api/decline-request/{id}': {
+      post: {
+        tags: ['Network'],
+        summary: 'Decline incoming connection request',
+        security: [
+          { cookieAuth: [] }
+        ],
+        parameters: [
+          {
+            name: 'id',
+            in: 'path',
+            required: true,
+            schema: {
+              type: 'string'
+            }
+          }
+        ],
+        responses: {
+          '200': {
+            description: 'Request declined'
+          },
+          '401': {
+            description: 'Unauthorized request'
+          },
+          '500': {
+            description: 'Failed to decline request'
+          }
+        }
+      }
+    },
+    '/api/disconnect/{id}': {
+      post: {
+        tags: ['Network'],
+        summary: 'Remove connection',
+        security: [
+          { cookieAuth: [] }
+        ],
+        parameters: [
+          {
+            name: 'id',
+            in: 'path',
+            required: true,
+            schema: {
+              type: 'string'
+            }
+          }
+        ],
+        responses: {
+          '200': {
+            description: 'Connection removed successfully'
+          },
+          '401': {
+            description: 'Unauthorized request'
+          },
+          '404': {
+            description: 'Connection not found'
+          },
+          '500': {
+            description: 'Failed to remove connection'
+          }
+        }
+      }
+    },
+    '/api/chat/history/{userId}': {
+      get: {
+        tags: ['Chat'],
+        summary: 'Get chat history',
+        security: [
+          { cookieAuth: [] }
+        ],
+        parameters: [
+          {
+            name: 'id',
+            in: 'path',
+            required: true,
+            schema: {
+              type: 'string'
+            }
+          }
+        ],
+        responses: {
+          '200': {
+            description: 'Chat history retrieved successfully',
+            content: {
+              'application/json': {
+                schema: {
+                  type: 'object',
+                  properties: {
+                    success: { type: 'boolean' },
+                    message: { type: 'string' },
+                    body: {
+                      $ref: '#/components/schemas/Chat'
+                    }
+                  }
+                }
+              }
+            }
+          },
+          '401': {
+            description: 'Unauthorized request'
+          },
+          '500': {
+            description: 'Failed to fetch chat history'
           }
         }
       }
